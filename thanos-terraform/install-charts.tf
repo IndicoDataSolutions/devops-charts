309d8b8cebec7e9da7505900376a5aeb5fc26f1c
Eric Fontana <eric.fontana@indico.io>
2023-11-29 09:02:27 -0500

    Remove unused dependencies

--- thanos-terraform/install-charts.tf
+++ thanos-terraform/install-charts.tf
@@ -1,11 +1,4 @@
 resource "helm_release" "thanos-pre-reqs" {
-  depends_on = [
-    vault_kubernetes_auth_backend_config.vault-auth,
-    vault_kubernetes_auth_backend_role.vault-auth-role,
-    kubernetes_secret_v1.vault-auth-default,
-    kubernetes_service_account_v1.vault-auth-default,
-    kubernetes_cluster_role_binding.vault-auth
-  ]
   name              = "tpr"
   chart             = "thanos-pre-reqs"
   repository        = "./helm-charts"
