3bca9dc36f326306ce69575735413ce3cf2dbb4a
Eric Fontana <eric.fontana@indico.io>
2023-11-29 11:47:14 -0500

    Move overrides to the tf so it runs

--- thanos-terraform/install-charts.tf
+++ thanos-terraform/install-charts.tf
@@ -10,7 +10,57 @@ resource "helm_release" "thanos" {
   name              = "t"
   chart             = "thanos"
   repository        = "./helm-charts"
-  force_update      = true
   dependency_update = true
+
+  values = [
+    <<EOF
+thanos:
+  enabled: true
+  existingObjstoreSecret: thanos-storage
+  storegateway:
+    enabled: true
+    sharded:
+      enabled: true
+    persistence:
+      enabled: true
+      size: 500Gi
+    args:
+      - store
+      - --log.level=info
+      - --log.format=logfmt
+      - --grpc-address=0.0.0.0:10901
+      - --http-address=0.0.0.0:10902
+      - --data-dir=/data
+      - --objstore.config-file=/conf/thanos_storage.yaml
+      
+grafana:
+  service:
+    annotations:
+      external-dns.alpha.kubernetes.io/hostname: grafana.monitoring.us-east-2.indico-devops.indico.io
+  ingress:
+    enabled: true
+    annotations:
+      cert-manager.io/cluster-issuer: zerossl
+    labels:
+      acme.cert-manager.io/dns01-solver: "true"
+    hosts:
+      - grafana.monitoring.us-east-2.indico-devops.indico.io
+    tls:
+      - secretName: t-grafana-tls
+        hosts:
+          - grafana.monitoring.us-east-2.indico-devops.indico.io
+  admin:
+    passwordKey: admin-password
+    userKey: admin-user
+  datasources:
+    datasources.yaml:
+      apiVersion: 1
+      datasources:
+      - name: Prometheus
+        type: prometheus
+        url: http://t-thanos-query:9090
+        access: proxy
+EOF
+  ]
 }
 
