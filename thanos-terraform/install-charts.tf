b47f1481d0d6a102521cc5762978b86827142cf8
Eric Fontana <eric.fontana@indico.io>
2023-11-29 04:08:54 -0500

    Install thanos

--- thanos-terraform/install-charts.tf
+++ thanos-terraform/install-charts.tf
@@ -4,3 +4,12 @@ resource "helm_release" "thanos-pre-reqs" {
   repository        = "./helm-charts"
   dependency_update = true
 }
+
+resource "helm_release" "thanos" {
+  depends_on        = [helm_release.thanos-pre-reqs]
+  name              = "thanos"
+  chart             = "thanos"
+  repository        = "./helm-charts"
+  dependency_update = true
+}
+
